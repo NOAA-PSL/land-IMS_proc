5536ebdbc251912ed8b2b94db132cca5b16501a6
yuanxue2870 <yuan.xue@noaa.gov>
2025-01-06 15:28:03 +0000

    revert 'snodl' to 'snwdph'

--- sorc/SCFaggregate_mod.f90
+++ sorc/SCFaggregate_mod.f90
@@ -670,10 +670,10 @@ subroutine calculate_scf(idim, jdim, source, otype, yyyymmddhh, jdate, fcst_path
             swefcs(:,:,t) = dummy
 
             ! snow depth
-            error=nf90_inq_varid(ncid, "snodl", id_var)
-            call netcdf_err(error, 'reading snodl id' )
+            error=nf90_inq_varid(ncid, "snwdph", id_var)
+            call netcdf_err(error, 'reading snwdph id' )
             error=nf90_get_var(ncid, id_var, dummy)
-            call netcdf_err(error, 'reading snodl' )
+            call netcdf_err(error, 'reading snwdph' )
             sndfcs(:,:,t) = dummy
 
             ! layer 1 soil temperature
